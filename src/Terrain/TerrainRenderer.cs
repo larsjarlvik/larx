edc5984e402bcbef2eb06275d1b0bddbb3a18df9
larsjarlvik <lars.jarlvik@gmail.com>
2018-12-08 16:20:51 +0700

    Use VBO

--- src/Terrain/TerrainRenderer.cs
+++ src/Terrain/TerrainRenderer.cs
@@ -1,3 +1,4 @@
+using System;
 using OpenTK;
 using OpenTK.Graphics.OpenGL;
 
@@ -16,23 +17,23 @@ namespace Larx.Terrain
         public void Render(Camera camera)
         {
             camera.ApplyCamera(Shader);
-            GL.DrawArrays(PrimitiveType.TriangleStrip, 0, 4);
+            GL.DrawElements(PrimitiveType.Triangles, 6, DrawElementsType.UnsignedShort, IntPtr.Zero);
         }
 
         private void build()
         {
             var triangleArray = GL.GenVertexArray();
             GL.BindVertexArray(triangleArray);
-            var positions = new Vector3[] {
-                new Vector3(-0.5f, -0.5f, 0.0f),
-                new Vector3(-0.5f,  0.5f, 0.0f),
-                new Vector3( 0.5f, -0.5f, 0.0f),
-                new Vector3( 0.5f,  0.5f, 0.0f)
+            var vertices = new Vector3[] {
+                new Vector3(-0.5f, -0.5f, 0.0f), // Bottom left
+                new Vector3( 0.5f, -0.5f, 0.0f), // Bottom right
+                new Vector3( 0.5f,  0.5f, 0.0f), // Top right
+                new Vector3(-0.5f,  0.5f, 0.0f)  // Top left
             };
 
-            var positionBuffer = GL.GenBuffer();
-            GL.BindBuffer(BufferTarget.ArrayBuffer, positionBuffer);
-            GL.BufferData<Vector3>(BufferTarget.ArrayBuffer, positions.Length * Vector3.SizeInBytes, positions, BufferUsageHint.StaticDraw);
+            var vertexBuffer = GL.GenBuffer();
+            GL.BindBuffer(BufferTarget.ArrayBuffer, vertexBuffer);
+            GL.BufferData<Vector3>(BufferTarget.ArrayBuffer, vertices.Length * Vector3.SizeInBytes, vertices, BufferUsageHint.StaticDraw);
             GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 0, 0);
             GL.EnableVertexAttribArray(0);
 
@@ -40,7 +41,7 @@ namespace Larx.Terrain
                 new Vector3(1.0f, 0.0f, 0.0f),
                 new Vector3(0.0f, 1.0f, 0.0f),
                 new Vector3(0.0f, 0.0f, 1.0f),
-                new Vector3(1.0f, 0.0f, 0.0f)
+                new Vector3(1.0f, 1.0f, 0.0f)
             };
 
             var colorBuffer = GL.GenBuffer();
@@ -48,6 +49,11 @@ namespace Larx.Terrain
             GL.BufferData<Vector3>(BufferTarget.ArrayBuffer, colors.Length * Vector3.SizeInBytes, colors, BufferUsageHint.StaticDraw);
             GL.VertexAttribPointer(1, 3, VertexAttribPointerType.Float, false, 0, 0);
             GL.EnableVertexAttribArray(1);
+
+            var indices = new ushort[] { 0, 1, 2, 0, 2, 3 };
+            var indexBuffer = GL.GenBuffer();
+            GL.BindBuffer(BufferTarget.ElementArrayBuffer, indexBuffer);
+            GL.BufferData(BufferTarget.ElementArrayBuffer, indices.Length * sizeof(ushort), indices, BufferUsageHint.StaticDraw);
         }
     }
 }
