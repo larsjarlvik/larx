6add74d0948f6e1e5a5833904db03ffc3ecd935d
larsjarlvik <lars.jarlvik@gmail.com>
2018-12-13 18:12:04 +0700

    Optimized normal calculation

--- src/Terrain/TerrainRenderer.cs
+++ src/Terrain/TerrainRenderer.cs
@@ -55,40 +55,80 @@ namespace Larx.Terrain
             GL.DrawElements(PrimitiveType.Triangles, indexCount, DrawElementsType.UnsignedInt, IntPtr.Zero);
         }
 
-        public void ChangeElevation(float offset, MousePicker picker) {
+        public void ChangeElevation(float offset, MousePicker picker)
+        {
             var position = GetPosition(picker);
-            var index = getTileIndex(position);
-            if (index == null) return;
+            var radius = 3;
+            var toUpdate = getTilesInArea(position, radius);
 
-            var i = (int)index;
-            var elev = vertices[i].Y + offset;
+            foreach (var i in toUpdate)
+            {
+                if (i >= indices.Count) continue;
 
-            vertices[i] = new Vector3(vertices[i].X, elev, vertices[i].Z);
-            colors[i] = elev > 0f
-                ? new Vector3(1f, 1f - (elev / 2f), 1f - (elev / 2f))
-                : new Vector3(1f + (elev / 2f), 1f + (elev / 2f), 1f);
+                var vertex = vertices[(int)indices[(int)i]];
+                var distance = Math.Abs(Vector3.Distance(position, vertex));
 
-            calculateNormals();
-            updateBuffers();
+                var color = colors[(int)indices[(int)i]];
+                var elev = vertex.Y + (offset / distance / radius * 0.5f);
+
+                vertices[(int)indices[(int)i]] = new Vector3(vertex.X, elev, vertex.Z);
+                colors[(int)indices[(int)i]] = elev > 0f
+                    ? new Vector3(1f, 1f - (elev / 2f), 1f - (elev / 2f))
+                    : new Vector3(1f + (elev / 2f), 1f + (elev / 2f), 1f);
+
+                updateNormals(position, radius + 1);
+                updateBuffers();
+            }
+        }
+
+        private void updateNormals(Vector3 center, float radius)
+        {
+            var toUpdate = getTilesInArea(center, radius);
+
+            for (var i = 0; i < toUpdate.Count; i++)
+            {
+                var ci = toUpdate[i];
+                if (ci < 0 || ci >= indices.Count) continue;
+                updateNormal(ci);
+            }
         }
 
         private void calculateNormals()
         {
-            for (int i = 0; i < indexCount; i += 3)
-            {
-                var v1 = vertices[(int)indices[i]];
-                var v2 = vertices[(int)indices[i + 1]];
-                var v3 = vertices[(int)indices[i + 2]];
+            for (var i = 0; i < indices.Count; i += 3)
+                updateNormal(i);
 
-                normals[(int)indices[i]] += Vector3.Cross(v2 - v1, v3 - v1);
-                normals[(int)indices[i + 1]] += Vector3.Cross(v2 - v1, v3 - v1);
-                normals[(int)indices[i + 2]] += Vector3.Cross(v2 - v1, v3 - v1);
-            }
+            updateBuffers();
+        }
 
-            foreach(var normal in normals)
-            {
-                normal.Normalize();
-            }
+        private List<int> getTilesInArea(Vector3 center, float radius)
+        {
+            var included = new List<int>();
+
+            for (var z = center.Z - radius; z <= center.Z + radius; z++)
+                for (var x = center.X - radius; x <= center.X + radius; x++)
+                {
+                    var index = getTileIndex(new Vector3(x, 0, z));
+                    if (index == null) continue;
+                    included.Add((int)index);
+                }
+
+            return included;
+        }
+
+        private void updateNormal(int i)
+        {
+            var v1 = vertices[(int)indices[i]];
+            var v2 = vertices[(int)indices[i + 1]];
+            var v3 = vertices[(int)indices[i + 2]];
+
+            normals[(int)indices[i]] += Vector3.Cross(v2 - v1, v3 - v1);
+            normals[(int)indices[i + 1]] += Vector3.Cross(v2 - v1, v3 - v1);
+            normals[(int)indices[i + 2]] += Vector3.Cross(v2 - v1, v3 - v1);
+
+            normals[(int)indices[i]].Normalize();
+            normals[(int)indices[i + 1]].Normalize();
+            normals[(int)indices[i + 2]].Normalize();
         }
 
         private void build()
@@ -100,20 +140,23 @@ namespace Larx.Terrain
             var rnd = new Random();
             var i = 0;
 
-            for(var z = -halfMapSize; z <= halfMapSize; z++) {
-                for(var x = -halfMapSize; x <= halfMapSize; x++) {
+            for (var z = -halfMapSize; z <= halfMapSize; z++)
+            {
+                for (var x = -halfMapSize; x <= halfMapSize; x++)
+                {
                     vertices.Add(new Vector3(x, 0f, z));
                     colors.Add(new Vector3(1f, 1f, 1f));
                     normals.Add(new Vector3(0f, 1f, 0f));
 
-                    if (x < halfMapSize && z < halfMapSize) {
+                    if (x < halfMapSize && z < halfMapSize)
+                    {
                         indices.AddRange(new uint[] {
                             (uint)(i), (uint)(i + mapSize + 1), (uint)(i + 1),
                             (uint)(i + 1), (uint)(i + mapSize + 1), (uint)(i + mapSize + 2)
                         });
                     }
 
-                    i ++;
+                    i++;
                 }
             }
 
@@ -139,7 +182,7 @@ namespace Larx.Terrain
             var z = (int)Math.Round(position.Z + (mapSize / 2));
             if (z < 0 || z > mapSize) return null;
 
-            return (z * (mapSize + 1)) + x;
+            return ((z * mapSize) + x) * 6;
         }
 
         private void updateBuffers()
