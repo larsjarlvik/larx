2fe01f8f135158fd1f5c2ee49db3aa9a0bb2b054
larsjarlvik <lars.jarlvik@gmail.com>
2019-09-17 18:12:23 +0700

    Fixed some issues with terrain picking

--- src/Terrain/TerrainPicker.cs
+++ src/Terrain/TerrainPicker.cs
@@ -4,7 +4,7 @@ using OpenTK;
 namespace Larx.Terrain
 {
     public class TerrainPicker {
-        private const float RayRange = 2000;
+        private const float RayRange = 1000;
         private const int MaxRecursions = 300;
         private readonly TerrainRenderer renderer;
 
@@ -25,24 +25,19 @@ namespace Larx.Terrain
                 return picker.GetPointOnRay(half);
             }
 
-            if (intersectionInRange(start, half, picker)) {
-                return findPosition(count + 1, start, half, picker);
-            } else {
-                return findPosition(count + 1, half, finish, picker);
-            }
+            return intersectionInRange(start, half, picker)
+                ? findPosition(count + 1, start, half, picker)
+                : findPosition(count + 1, half, finish, picker);
         }
 
         private bool intersectionInRange(float start, float finish, MousePicker picker)
         {
-            var startPoint = picker.GetPointOnRay(start);
             var endPoint = picker.GetPointOnRay(finish);
-
-            var startElev = renderer.GetElevationAtPoint(startPoint);
             var endElev = renderer.GetElevationAtPoint(endPoint);
 
-            if (startElev == null || endElev == null) return true;
+            if (endElev == null) endElev = 0.0f;
 
-            return startPoint.Y > (float)startElev && endPoint.Y < (float)endElev;
+            return endPoint.Y < (float)endElev;
         }
     }
 }
